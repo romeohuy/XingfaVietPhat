949bf6984bfeb6b813ca781c2c68c53adc6d3f1a
Huy Nguyen Ngoc <romeohuy@gmail.com>
2019-02-19 11:06:04 +0700

    * init source code

---
+++ src/WebXingFaUmbraco/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace WebXingFaUmbraco.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PATH_PARTIAL_VIEW = "~/Views/Partials/Home";
+        // GET: Home
+        public ActionResult RenderSliderImage()
+        {
+            return PartialView($"{PATH_PARTIAL_VIEW}/SliderImage.cshtml");
+        }
+    }
+}
---
+++ src/WebXingFaUmbraco/Controllers/SiteSharedLayoutController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace WebXingFaUmbraco.Controllers
+{
+    public class SiteSharedLayoutController : SurfaceController
+    {
+        private const string PATH_PARTIAL_VIEW = "~/Views/Partials/SharedLayout";
+        // GET: SiteSharedLayout
+        public ActionResult RenderHeader()
+        {
+            return PartialView($"{PATH_PARTIAL_VIEW}/Header.cshtml");
+        }
+        public ActionResult RenderFooter()
+        {
+            return PartialView($"{PATH_PARTIAL_VIEW}/Footer.cshtml");
+        }
+    }
+}
